2e826497f18134b9c48b502c44dad25f65f0a86c
Eumnya <2124524@naver.com>
2024-01-25 17:50:05 +0900

    Docs: 주석 추가

--- src/main/java/org/example/springbootdeveloper/Member.java
+++ src/main/java/org/example/springbootdeveloper/Member.java
@@ -6,16 +6,17 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 2) 기본 생성자
 @AllArgsConstructor
 @Getter
-@Entity
+@Entity // 1) 엔티티로 지정
 public class Member {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Id // 3) id 필드를 기본키로 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 4) 기본 키를 자동으로 1씩 증가
     @Column(name = "id", updatable = false)
     private Long id; // DB 테이블의 'id' 컬럼과 매칭
 
-    @Column(name = "name", nullable = false)
+    @Column(name = "name", nullable = false) // 5) name 이라는 not null 컬럼과 매핑
     private String name; // DB 테이블의 'name' 컬럼과 매칭
 }
