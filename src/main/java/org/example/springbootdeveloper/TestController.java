71a85853394466f1acb1f0079ae84ce60f57f6d9
Eumnya <2124524@naver.com>
2024-01-25 14:51:58 +0900

    Fix: 실행 안되는 오류 수정 -> yml 문단 배치 문제 해결

--- src/main/java/org/example/springbootdeveloper/TestController.java
+++ src/main/java/org/example/springbootdeveloper/TestController.java
@@ -3,8 +3,6 @@ package org.example.springbootdeveloper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.lang.reflect.Member;
 import java.util.List;
 
 @RestController
