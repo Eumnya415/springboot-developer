49dddbb0570ddc0c334e1c7cc0b11a3ac4f30144
Eumnya <2124524@naver.com>
2024-01-25 13:24:35 +0900

    Docs: 주석 추가

--- src/main/java/org/example/springbootdeveloper/SpringBootDeveloperApplication.java
+++ src/main/java/org/example/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -3,9 +3,10 @@ package org.example.springbootdeveloper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // 스프링 부트 사용에 필요한 기본 설정
 public class SpringBootDeveloperApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootDeveloperApplication.class, args);
+        // SpringApplication.run() 메서드 : 애플리케이션 실행
     }
 }
