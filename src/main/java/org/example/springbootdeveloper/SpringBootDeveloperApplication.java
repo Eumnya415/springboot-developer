4b165be810f634c29723578f5e4eadda4059b83d
Eumnya <2124524@naver.com>
2024-01-30 14:52:25 +0900

    Add: 엔티티의 생성시간, 수정시간 자동 업데이트 위한 애너테이션 추가

--- src/main/java/org/example/springbootdeveloper/SpringBootDeveloperApplication.java
+++ src/main/java/org/example/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -2,7 +2,9 @@ package org.example.springbootdeveloper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // created_at, updated_at 자동 업데이트
 @SpringBootApplication // 스프링 부트 사용에 필요한 기본 설정
 public class SpringBootDeveloperApplication {
     public static void main(String[] args) {
