1756922bdef465e489d525b214ed6ab996192612
CHA <2124524@gmail.com>
2024-02-02 10:47:46 +0900

    Add: 블로그 글 반환할 컨트롤러 작성

--- src/main/java/org/example/springbootdeveloper/controller/BlogViewController.java
+++ src/main/java/org/example/springbootdeveloper/controller/BlogViewController.java
@@ -2,10 +2,13 @@ package org.example.springbootdeveloper.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.example.springbootdeveloper.DTO.ArticleListViewResponse;
+import org.example.springbootdeveloper.DTO.ArticleViewResponse;
+import org.example.springbootdeveloper.domain.Article;
 import org.example.springbootdeveloper.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -23,5 +26,13 @@ public class BlogViewController {
         model.addAttribute("articles", articles); // 1) 블로그 글 리스트 저장
 
         return "articleList"; // 2) articleList.html 라는 뷰 조회
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
     }
 }
